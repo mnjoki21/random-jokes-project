1a66bb079ab22ecabbf7e4c9c8a546cfbf092e25
mitchelle <mitchellenjoki7@gmail.com>
2022-06-22 11:35:56 +0300

    jokes

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'f744cd80ddmshd282901e93a5739p1281c5jsn8ed87d8aeeba',
-		'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
-	}
-};
 
-fetch('https://dad-jokes.p.rapidapi.com/random/joke', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+fetch("https://v2.jokeapi.dev/joke/Programming,Dark,Pun")
+.then((response) => response.json()) 
+.then((data)=> console.log (data) )
+.catch 
+
