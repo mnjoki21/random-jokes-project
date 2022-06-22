95bd3f476e38514542268ae1c4f88128a22d6574
mitchelle <mitchellenjoki7@gmail.com>
2022-06-22 19:06:57 +0300

    jokes

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const getJoke = document.getElementById('joke')
+const jokeButton = document.getElementById('joke-btn')
+const url = "https://v2.jokeapi.dev/joke/Programming,Dark,Pun?type=single"
 
 
 
