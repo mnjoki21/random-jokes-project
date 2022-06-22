3d27070cdb72520dcb57adfd4e219289cab8801d
mitchelle <mitchellenjoki7@gmail.com>
2022-06-22 19:58:45 +0300

    jokes

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const getJoke = document.getElementById('joke')
+const joke = document.getElementById('joke')
 const jokeButton = document.getElementById('joke-btn')
 const url = "https://v2.jokeapi.dev/joke/Programming,Dark,Pun?type=single"
+
+let getJoke = () =>{
+	fetch(url)
+	.then(data => data.json())
+	.then(item => 
+		joke.textContent = `${item.joke}`
+	)
+
+} 
+jokeButton.addEventListener("click",getJoke)
+
+getJoke ()
 
 
 
