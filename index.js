6e9354ee6bc59b8d4450982f36a5c0f57e048d6c
mitchelle <mitchellenjoki7@gmail.com>
2022-06-22 19:03:08 +0300

    jokes

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
 
 
 
-const button = document.getElementsByClassName('joke-btn')
-const jokeText = document.getElementsByClassName ('text2') 
-
-
-joke-btn.addEventListener('click' , getJoke) 
-getJoke()
-
-async function getJoke(){
-	//calls the api using fetch 
-	const jokesResponse = await fetch('https://v2.jokeapi.dev/joke/Programming,Dark,Pun' , {
-		headers: {
-			accept : 'application/json'
-		}
-	}) 
-	
-	//getting the function 
-
-} 
-
-const joke = await jokesResponse.json()
-console.log (jokes) 
-
-jokeEl.innerHTML=joke.joke
-
-
 
 
