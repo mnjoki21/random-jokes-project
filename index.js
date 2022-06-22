a2036e5da638d1d949133ea2107f816e438afafc
mitchelle <mitchellenjoki7@gmail.com>
2022-06-22 15:52:29 +0300

    jokes

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 
-fetch("https://v2.jokeapi.dev/joke/Programming,Dark,Pun")
-.then((response) => response.json()) 
-.then((data)=> console.log (data) )
-.catch 
+
+
+const button = document.getElementsByClassName('joke-btn')
+const jokeText = document.getElementsByClassName ('text2') 
+
+
+joke-btn.addEventListener('click' , getJoke) 
+getJoke()
+
+async function getJoke(){
+	//calls the api using fetch 
+	const jokesResponse = await fetch('https://v2.jokeapi.dev/joke/Programming,Dark,Pun' , {
+		headers: {
+			accept : 'application/json'
+		}
+	}) 
+	
+	//getting the function 
+
+} 
+
+const joke = await jokesResponse.json()
+console.log (jokes) 
+
+jokeEl.innerHTML=joke.joke
+
+
+
 
