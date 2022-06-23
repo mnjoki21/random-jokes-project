304bf14c99378a07aac5555191205f262724ce30
mitchelle <mitchellenjoki7@gmail.com>
2022-06-23 11:31:16 +0300

    jokes

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const joke = document.getElementById('joke')
-const jokeButton = document.getElementById('joke-btn')
-const url = "https://v2.jokeapi.dev/joke/Programming,Dark,Pun?type=single"
+const like = document.getElementById("countLikes");
+const likeBtn = document.getElementById("btn");
+const joke = document.getElementById("joke");
+const jokeButton = document.getElementById("joke-btn");
+const url = "https://v2.jokeapi.dev/joke/Programming,Dark,Pun?type=single";
 
-let getJoke = () =>{
-	fetch(url)
-	.then(data => data.json())
-	.then(item => 
-		joke.textContent = `${item.joke}`
-	)
+let counter = 0;
 
-} 
-jokeButton.addEventListener("click",getJoke)
+let getJoke = () => {
+  fetch(url)
+    .then((data) => data.json())
+    .then((item) => (joke.textContent = `${item.joke}`));
+};
+jokeButton.addEventListener("click", getJoke);
 
-getJoke ()
+// getJoke();
+let incrementCount = () => {
+  counter++;
+  // for (let count = 0; count <= 10; count++)
+  console.log(counter);
+  likeBtn.innerHTML = counter;
 
+};
+likeBtn.addEventListener("click", incrementCount);
 
-
-
-
+// document.getElementById("btn").addEventListener("click", ()=>{
+//   incrementCount});
